0af3cbfc88a621af7204e2d345fb77af187c226a
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-03-13 08:34:11 +0100

    Added fonts

---
+++ Homework_3/app/src/main/java/com/example/homework_3/ui/home/AddProductFragment.kt
@@ -0,0 +1,60 @@
+package com.example.homework_3.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.core.view.children
+import androidx.lifecycle.ViewModelProvider
+import com.example.homework_3.model.Product
+import com.example.homework_3.viewmodels.ProductsViewModel
+import com.example.homework_3.R
+
+class AddProductFragment : Fragment() {
+
+    private lateinit var viewModel: ProductsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_product, container, false)
+
+        // Initialize the ViewModel
+        viewModel = ViewModelProvider(requireActivity())[ProductsViewModel::class.java]
+
+        val submitButton = view.findViewById<Button>(R.id.btn_submit)
+        val formContainer = view.findViewById<LinearLayout>(R.id.form_container)
+
+        submitButton.setOnClickListener {
+            val name = view.findViewById<EditText>(R.id.editTextName).text.toString()
+            val description = view.findViewById<EditText>(R.id.editTextDescription).text.toString()
+            val brand = view.findViewById<EditText>(R.id.editTextBrand).text.toString()
+            val category = view.findViewById<EditText>(R.id.editTextCategory).text.toString()
+            val productType = view.findViewById<EditText>(R.id.editTextProductType).text.toString()
+            val style = view.findViewById<EditText>(R.id.editTextStyle).text.toString()
+            val color = view.findViewById<EditText>(R.id.editTextColor).text.toString()
+            val material = view.findViewById<EditText>(R.id.editTextMaterial).text.toString()
+            val size = view.findViewById<EditText>(R.id.editTextSize).text.toString()
+            val price = view.findViewById<EditText>(R.id.editTextPrice).text.toString()
+
+            val newProduct = Product(name, description, brand, category,
+                productType, style, color, material,
+                size, price)
+
+            viewModel.addProduct(product = newProduct)
+
+            // Reset the input fields
+            formContainer.children.filterIsInstance<EditText>().forEach {
+                it.text.clear()
+            }
+        }
+
+        return view
+    }
+
+}
---
+++ Homework_3/app/src/main/java/com/example/homework_3/ui/products/ProductsFragment.kt
@@ -0,0 +1,47 @@
+package com.example.homework_3.ui.products
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.example.homework_3.model.Product
+import com.example.homework_3.viewmodels.ProductsViewModel
+import com.example.homework_3.R
+
+class ProductsFragment : Fragment() {
+
+    private lateinit var viewModel: ProductsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_products, container, false)
+
+        // Initialize the ViewModel
+        viewModel = ViewModelProvider(requireActivity())[ProductsViewModel::class.java]
+
+        val productsContainer = view.findViewById<LinearLayout>(R.id.productsContainer)
+
+        // Products observer which updates the UI
+        val productsObserver = Observer<ArrayList<Product>> { products ->
+            // Update the UI when the product list changes
+            productsContainer.removeAllViews()
+            products.forEach { product ->
+                val textView = TextView(context)
+                textView.text = product.toString()
+                productsContainer.addView(textView)
+            }
+        }
+
+        // Observe the LiveData, passing in main activity as the LifecycleOwner and the observer.
+        viewModel.products.observe(viewLifecycleOwner, productsObserver)
+
+        return view
+    }
+}
