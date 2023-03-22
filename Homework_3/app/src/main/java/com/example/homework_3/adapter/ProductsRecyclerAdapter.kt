8facd48c7c10a09dbbe572a404c61fb84a9997c7
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-03-22 02:46:46 +0100

    Remove unnecessay lines

--- Homework_3/app/src/main/java/com/example/homework_3/adapter/ProductsRecyclerAdapter.kt
+++ Homework_3/app/src/main/java/com/example/homework_3/adapter/ProductsRecyclerAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.homework_3.adapter
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,6 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.homework_3.R
 import com.example.homework_3.databinding.ProductItemBinding
-import com.example.homework_3.enum.SizeEnum
 import com.example.homework_3.model.Product
 
 class ProductsRecyclerAdapter(private val context: Context, private val productsList: ArrayList<Product>):
@@ -26,17 +24,7 @@ class ProductsRecyclerAdapter(private val context: Context, private val products
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val product = productsList[position]
-        holder.binding.root.backgroundTintList = ColorStateList.valueOf(
-            when(product.size) {
-                SizeEnum.XXS -> context.getColor(R.color.purple_200)
-                SizeEnum.XS -> context.getColor(R.color.purple_200)
-                SizeEnum.S -> context.getColor(R.color.purple_200)
-                SizeEnum.M -> context.getColor(R.color.purple_200)
-                SizeEnum.L -> context.getColor(R.color.purple_200)
-                SizeEnum.XL -> context.getColor(R.color.purple_200)
-                SizeEnum.XXL -> context.getColor(R.color.purple_200)
-            }
-        )
+
         holder.binding.apply {
             productName.text = product.name
             productDescription.text = product.description
