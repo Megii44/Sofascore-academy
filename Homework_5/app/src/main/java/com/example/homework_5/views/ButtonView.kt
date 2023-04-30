2db2aec8c5940fecd976e17fe2bd3bdcc69349b7
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-04-30 22:09:50 +0200

    Clear lists on settings and fixed setting input layout

--- Homework_5/app/src/main/java/com/example/homework_5/views/ButtonView.kt
+++ Homework_5/app/src/main/java/com/example/homework_5/views/ButtonView.kt
@@ -1,20 +1,16 @@
 package com.example.homework_5.views
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.os.Build
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.homework_5.R
 import com.example.homework_5.databinding.ButtonViewBinding
 
-
-@RequiresApi(Build.VERSION_CODES.P)
 class ButtonView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
-    private val binding: ButtonViewBinding
+    private var binding: ButtonViewBinding
 
     private val text: String
     private val backgroundColor: Int
@@ -29,14 +25,13 @@ class ButtonView(context: Context, attrs: AttributeSet) : ConstraintLayout(conte
             R.styleable.ButtonView, 0, 0
         ).apply {
             try {
-                text = R.styleable.ButtonView_text.toString()
-                backgroundColor = R.styleable.ButtonView_background_color
-                borderColor = R.styleable.ButtonView_border_color
-                disabled = false
+                text = getString(R.styleable.ButtonView_text) ?: ""
+                backgroundColor = getColor(R.styleable.ButtonView_background_color, 0)
+                borderColor = getColor(R.styleable.ButtonView_border_color, 0)
+                disabled = getBoolean(R.styleable.ButtonView_disabled, false)
 
                 setText(text)
-                setBackgroundColor(backgroundColor)
-                setBorderColor(borderColor)
+                setBorderDrawable(borderColor, backgroundColor)
                 setDisabled(disabled)
             } finally {
                 recycle()
@@ -45,19 +40,19 @@ class ButtonView(context: Context, attrs: AttributeSet) : ConstraintLayout(conte
     }
 
     private fun setText(text: String) {
-        binding.button.text = text
+        binding.customButton.text = text
     }
 
-    override fun setBackgroundColor(color: Int) {
-        binding.button.backgroundTintList = ColorStateList.valueOf(color)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun setBorderColor(color: Int) {
-        binding.button.outlineAmbientShadowColor = color
+    private fun setBorderDrawable(borderColor: Int, backgroundColor: Int) {
+        val drawable = GradientDrawable().apply {
+            setColor(backgroundColor)
+            setStroke(2, borderColor)
+            cornerRadius = 4f
+        }
+        //binding.customButton.background = drawable
     }
 
     private fun setDisabled(disabled: Boolean) {
-        binding.button.isActivated = disabled
+        //binding.customButton.isEnabled = !disabled
     }
 }
