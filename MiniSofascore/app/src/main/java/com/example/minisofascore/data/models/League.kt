cea448dd743af372eae33b2f5274edaa09becc3e
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-05-23 23:00:11 +0200

    Remove unnecessary files

--- MiniSofascore/app/src/main/java/com/example/minisofascore/data/models/League.kt
+++
@@ -1,9 +0,0 @@
-package com.example.minisofascore.data.models
-
-import android.graphics.drawable.Drawable
-
-data class League (
-    val name: String,
-    val icon: Drawable,
-    val contestants: List<String>
-)
