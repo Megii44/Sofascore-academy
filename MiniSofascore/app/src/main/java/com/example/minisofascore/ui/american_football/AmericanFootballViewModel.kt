83a503df65745d83204ca55999018ba02b417f49
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-06-06 16:38:37 +0200

    Delete unnecessary fragments

--- MiniSofascore/app/src/main/java/com/example/minisofascore/ui/american_football/AmericanFootballViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.minisofascore.ui.american_football
-
-import androidx.lifecycle.ViewModel
-
-class AmericanFootballViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- MiniSofascore/app/src/main/java/com/example/minisofascore/ui/basketball/BasketballViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.minisofascore.ui.basketball
-
-import androidx.lifecycle.ViewModel
-
-class BasketballViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
