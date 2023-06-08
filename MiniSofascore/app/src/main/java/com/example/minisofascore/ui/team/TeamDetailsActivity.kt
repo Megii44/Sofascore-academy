6d06df3e845113c5c4a57da8a1dfa55be0c6342e
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-06-08 02:48:51 +0200

    App bar for team details

--- MiniSofascore/app/src/main/java/com/example/minisofascore/ui/team/TeamDetailsActivity.kt
+++ MiniSofascore/app/src/main/java/com/example/minisofascore/ui/team/TeamDetailsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.minisofascore.ui.team
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.bumptech.glide.Glide
@@ -39,21 +42,43 @@ class TeamDetailsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        // Set back arrow icon color to white
+        val upArrow = ContextCompat.getDrawable(this, R.drawable.ic_back_arrow)
+        val colorWhite = ContextCompat.getColor(this, R.color.white)
+        upArrow?.let {
+            DrawableCompat.setTint(it, colorWhite)
+            DrawableCompat.setTintMode(it, PorterDuff.Mode.SRC_ATOP)
+            supportActionBar?.setHomeAsUpIndicator(it)
+        }
+
         // Set toolbar title
         val title = team?.name
-        val actionBarTitle = actionBarLayout.findViewById<View>(R.id.teamNameTextView)
+        val actionBarTitle = actionBarLayout.findViewById<View>(R.id.team_name_text_view)
         if (actionBarTitle is TextView) {
             actionBarTitle.text = title
+        }
+
+        val country = team?.country?.name
+        val actionBarCountry = actionBarLayout.findViewById<View>(R.id.country_text_view)
+        if (actionBarCountry is TextView) {
+            actionBarCountry.text = country
         }
 
         // Set toolbar image
         val teamLogoUrl = "https://academy.dev.sofascore.com/team/${team?.id}/image"
 
-        val actionBarImage = actionBarLayout.findViewById<View>(R.id.teamLogoImageView)
+        val actionBarImage = actionBarLayout.findViewById<View>(R.id.team_logo_image_view)
         if (actionBarImage is ImageView) {
             Glide.with(this)
-              .load(teamLogoUrl)
-            .into(actionBarImage)
+                .load(teamLogoUrl)
+                .into(actionBarImage)
+        }
+
+        val countryImage = actionBarLayout.findViewById<View>(R.id.country_logo_image_view)
+        if (countryImage is ImageView) {
+            Glide.with(this)
+                .load(teamLogoUrl)
+                .into(countryImage)
         }
 
         val viewPager = binding.viewPager
