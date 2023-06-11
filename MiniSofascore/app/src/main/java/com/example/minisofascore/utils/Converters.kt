957ce1acec6ecc26f783ecf999df39e39e0f0563
Magda Smolić - Ročak <magdarocak42@gmail.com>
2023-06-11 10:33:13 +0200

    Update Converters.kt

--- MiniSofascore/app/src/main/java/com/example/minisofascore/utils/Converters.kt
+++ MiniSofascore/app/src/main/java/com/example/minisofascore/utils/Converters.kt
@@ -2,6 +2,7 @@ package com.example.minisofascore.utils
 
 import androidx.room.TypeConverter
 import com.example.minisofascore.data.models.Sport
+import com.example.minisofascore.data.models.Country
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -15,5 +16,16 @@ class Converters {
     fun toSport(sportString: String): Sport {
         val sportType = object : TypeToken<Sport>() {}.type
         return Gson().fromJson(sportString, sportType)
+    }
+
+    @TypeConverter
+    fun fromCountry(country: Country): String {
+        return Gson().toJson(country)
+    }
+
+    @TypeConverter
+    fun toCountry(countryString: String): Country {
+        val countryType = object : TypeToken<Country>() {}.type
+        return Gson().fromJson(countryString, countryType)
     }
 }
